08935efc848deb9b774468d749e8d71237b5d920
Akhila4144 <orugantiakhila34@gmail.com>
2021-12-07 18:08:15 +0530

    UC1 Done

---
+++ UserRegexExceptionHandlingDay21/Pattern.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegexExceptionHandlingDay21
+{
+    class Pattern
+    {
+        public static string Regex_EmailId = "^[A-Z0-9a-z]{1,}([.#$^][A-Za-z0-9]+)?[@][A-Za-z]{2,}[.][A-Za-z]{2,3}([.][a-zA-Z]{2})?$";
+        public bool ValidateEmailId(string EmailId)
+        {
+            return Regex.IsMatch(EmailId, Regex_EmailId);
+
+        }
+        public static void CheckEmail(string EmailId)
+        {
+            try
+            {
+                if (EmailId == String.Empty || EmailId == " ")
+                {
+                    throw new ExceptionHandling(ExceptionHandling.ExceptionType.NULL_FIELD, "Field is empty ");
+                }
+                else if (Regex.IsMatch(EmailId, Regex_EmailId) == false)
+                {
+                    throw new ExceptionHandling(ExceptionHandling.ExceptionType.INVALID_INPUT,
+                        "Invalid EmailId");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Field exception ={0}", e.Message);
+            }
+        }
+    }
+}
---
+++ UserRegexExceptionHandlingDay21/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UserRegexExceptionHandlingDay21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration  ");
+            string EmailId;
+            Console.Write("Enter  Email Id  :");
+            EmailId = Console.ReadLine();
+            Pattern.CheckEmail(EmailId);
+            Console.ReadKey();
+        }
+    }
+}
