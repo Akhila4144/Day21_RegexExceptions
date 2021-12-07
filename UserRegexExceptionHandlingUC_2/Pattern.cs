ed3105ad9360facd5f36c68d0ca0050fb6fe9ce2
Akhila4144 <orugantiakhila34@gmail.com>
2021-12-07 18:10:49 +0530

    UC2 Done

---
+++ UserRegexExceptionHandlingUC_2/Pattern.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegexExceptionHandlingUC_2
+{
+    class Pattern
+    {
+        public static string Regex_LastName = "^[A-Z][a-z]{2,}$";
+
+
+        public bool ValidateLastName(string LastName)
+        {
+            return Regex.IsMatch(LastName, Regex_LastName);
+
+        }
+        public static void checkLastName(string LastName)
+        {
+            try
+            {
+                if (LastName == String.Empty || LastName == " ")
+                {
+                    throw new ExceptionHandling(ExceptionHandling.ExceptionType.NULL_FIELD, "Field is empty ");
+                }
+                else if (Regex.IsMatch(LastName, Regex_LastName) == false)
+                {
+                    throw new ExceptionHandling(ExceptionHandling.ExceptionType.INVALID_INPUT,
+                        "Invalid LastName please check the first letter ");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Field exception ={0}", e.Message);
+            }
+
+        }
+    }
+}
+    
---
+++ UserRegexExceptionHandlingUC_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UserRegexExceptionHandlingUC_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration  ");
+            string Lastname;
+            Console.Write("Enter Last Name : ");
+            Lastname = Console.ReadLine();
+            Pattern.checkLastName(Lastname);
+            Console.ReadKey();
+        }
+    }
+}
