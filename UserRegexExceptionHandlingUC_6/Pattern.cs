13b4c87d1a1b0af3c8b19ecafa0b6783e8f02e00
Akhila4144 <orugantiakhila34@gmail.com>
2021-12-08 13:55:23 +0530

    UC6 Done

---
+++ UserRegexExceptionHandlingUC_6/Pattern.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegexExceptionHandlingUC_6
+{
+    public class Pattern
+    {
+        public static string Regex_Password = "^((?=.*[A-Z])(?=.{8,}))";
+        public bool ValidatePassword(string Password)
+        {
+            return Regex.IsMatch(Password, Regex_Password);
+
+        }
+        public static void CheckPassword(string Password)
+        {
+            try
+            {
+                if (Password == String.Empty || Password == " ")
+                {
+                    throw new ExceptionHandling(ExceptionHandling.ExceptionType.NULL_FIELD, "Field  is empty ");
+                }
+                else if (Regex.IsMatch(Password, Regex_Password) == false)
+                {
+                    throw new ExceptionHandling(ExceptionHandling.ExceptionType.INVALID_INPUT,
+                        "Invalid Password. It should  contain atleast one upper case  character ");
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Field exception ={0}", e.Message);
+            }
+        }
+    
+    }
+
+}
---
+++ UserRegexExceptionHandlingUC_6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UserRegexExceptionHandlingUC_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Password;
+            Console.Write("Enter Password : ");
+            Password = Console.ReadLine();
+            Pattern.CheckPassword(Password);
+
+
+            Console.ReadKey();
+        }
+    }
+    
+}
